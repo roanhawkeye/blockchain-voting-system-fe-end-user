54671f9e0da880a6224662636cece97ac05c6714
Daniel Martinez <roanhawkeye@gmail.com>
2018-03-14 23:13:04 -0600

    Install bulma, normalize.css and font-awesome

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import 'bulma/css/bulma.css';
+import 'font-awesome/css/font-awesome.css';
 
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
