96b38df4058505a7f068706a403edefb4551765d
Daniel Martinez <daniel_martinez@epam.com>
2018-03-15 16:42:17 -0600

    EPMLABSSUM-98 - Install fetch and create sample api to consume id

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,24 +1,45 @@
 import React from 'react';
 import './styles.scss';
+import { Redirect } from 'react-router-dom';
 
-const Movements = () => (
-  <section className="column aside hero is-fullheight movement-section">
-    <div className="container has-text-centered">
-      <h1 className="title is-size-4">Asset Tracking</h1>
-      <div className="movement-form">
-        <div className="field has-addons">
-          <div className="control has-50-vw">
-            <input className="input" type="text" placeholder="Find Assets by ID" />
-          </div>
-          <div className="control">
-            <button className="button is-primary">
-              Search
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-);
+import API from './../../utils/API';
 
-export default Movements;
+class Home extends React.Component {
+  state = {
+    redirectToReferrer: false
+  };
+
+  search() {
+    API.findAsset();
+  }
+
+  render() {
+      const { redirectToReferrer } = this.state;
+    
+      if (redirectToReferrer) {
+        return <Redirect to={{ pathname: '/summary' }} />;
+      }
+
+      return (
+        <section className="column aside hero is-fullheight movement-section">
+          <div className="container has-text-centered">
+            <h1 className="title is-size-4">Asset Tracking</h1>
+            <div className="movement-form">
+              <div className="field has-addons">
+                <div className="control has-50-vw">
+                  <input className="input" type="text" placeholder="Find Assets by ID" />
+                </div>
+                <div className="control">
+                  <button className="button is-primary" onClick={this.search()}>
+                    Search
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      );
+    }
+  }
+
+export default Home;
