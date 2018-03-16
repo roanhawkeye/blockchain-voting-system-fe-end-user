dfe70ef7589f2cb413c188b6b89e5eb8de49c240
Daniel Martinez <daniel_martinez@epam.com>
2018-03-16 16:55:19 -0600

    EPMLABSSUM-93 - Add hero as a container for transaction history component

--- src/components/Transactions/Transactions.jsx
+++ src/components/Transactions/Transactions.jsx
@@ -2,22 +2,28 @@ import React from 'react';
 import './styles.scss';
 
 const Transactions = () => (
-  <div className="container">
-    <h1 className="title is-size-4">Transactions</h1>
-    <div className="columns">
-      <div className="column">
-        Event history
+  <section class="hero is-info is-medium">
+    <div class="hero-head">
+        <h1 className="title is-size-4">Transactions</h1>
+        <div className="columns">
+          <div className="column">
+            Event history
+          </div>
+        </div>
+    </div>
+    <div class="hero-body">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-one-third">
+            <div className="title is-size-5">Asset tracking history</div>
+          </div>
+          <div className="column">
+            Second column
+          </div>
+        </div>
       </div>
     </div>
-    <div className="columns">
-      <div className="column is-one-third">
-        <div className="title is-size-5">Asset tracking history</div>
-      </div>
-      <div className="column">
-        Second column
-      </div>
-    </div>
-  </div>
+  </section>
 );
 
 export default Transactions;
