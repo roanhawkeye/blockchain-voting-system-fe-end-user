259eaf7b5bd0ef5b3c8112f50cd80422fed534b9
Daniel Martinez <roanhawkeye@gmail.com>
2018-03-18 15:56:03 -0600

    EPMLABSSUM-93 - Add some hints to transaction container

--- src/components/Transactions/Transactions.jsx
+++ src/components/Transactions/Transactions.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.scss';
 
 const Transactions = () => (
-  <section class="hero is-info is-medium">
+  <section class="hero is-white is-medium sub-container">
     <div class="hero-head">
         <h1 className="title is-size-4">Transactions</h1>
         <div className="columns">
@@ -16,9 +16,36 @@ const Transactions = () => (
         <div className="columns">
           <div className="column is-one-third">
             <div className="title is-size-5">Asset tracking history</div>
+            <div>
+              Firt image from register event.
+            </div>
           </div>
           <div className="column">
-            Second column
+            <div className="field is-horizontal">
+              <div className="field-label is-normal">
+                <label className="label">ID:</label>
+              </div>
+              <div className="field-body">
+                <div className="field detail-content">
+                  <p className="control is-expanded has-icons-left">
+                    Asset ID
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="field is-horizontal">
+              <div className="field-label is-normal">
+                <label className="label">Description:</label>
+              </div>
+              <div className="field-body">
+                <div className="field detail-content">
+                  <p className="control is-expanded has-icons-left">
+                    Description del asset, summary of first event, register event
+                  </p>
+                </div>
+              </div>
+            </div>
+          Eventos here
           </div>
         </div>
       </div>
